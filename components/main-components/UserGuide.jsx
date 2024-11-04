e783b08ed150aa90f37f097388a08bc6a723589d
David Ikenna <sankkakeii@gmail.com>
2024-11-04 17:22:07 +0100

    update user guide for merchant data entry

--- components/main-components/UserGuide.jsx
+++ components/main-components/UserGuide.jsx
@@ -18,14 +18,23 @@ const UserGuideModal = ({ showTutorial, setShowTutorial }) => (
                     </button>
                     <h2 className="text-lg font-bold mb-4">User Guide</h2>
                     <ul className="list-disc pl-5 space-y-2">
+
+                        <li>
+                            <strong>Register:</strong> Register with email and password you can remember.
+                        </li>
+
+                        <li>
+                            <strong>ALLOW LOCATION ACCESS, app will not work without it.</strong>.
+                        </li>
+
+                        <li>
+                            <strong>Login:</strong> Inform An official to activate your account.
+                        </li>
                         <li>
                             <strong>Check-In:</strong> Click &quot;Check In&quot; to confirm your attendance.
                         </li>
                         <li>
-                            <strong>Provide Feedback:</strong> Please fill out the feedback form before checking out.
-                        </li>
-                        <li>
-                            <strong>Check-Out:</strong> You can only check out after submitting your feedback.
+                            <strong>Merchant data:</strong> Please fill out the merchant Data form for each merchant you register.
                         </li>
                     </ul>
                 </div>
